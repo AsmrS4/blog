e9a12fbf1e7736f98f39ff8b4e4aabc10a3a1078
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-02-02 21:22:47 +0700

    added login/logout api

---
+++ src/api/user/user.js
@@ -0,0 +1,34 @@
+export const logoutUser = async() => {
+    let token = localStorage.getItem('token');
+    try {
+        const response = await fetch(`https://blog.kreosoft.space/api/account/logout`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'text/plain',
+                'Authorization': 'Bearer ' + token
+            },
+        })
+        return response;
+    } catch (error) {
+        console.error('Response error: '+ error)  
+    }
+}
+
+export const loginUser = async(data) => {
+    try {
+        const response = await fetch(`https://blog.kreosoft.space/api/account/login`,{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                ...data
+            })
+        })
+        return response;
+    } catch (error) {
+        console.error('Response error: '+ error)
+    }
+}
