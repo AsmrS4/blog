ad7fda9255e07eb196d7242c44ba07b4e81a8d31
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-02-07 04:36:20 +0700

    added add/remove like request

--- src/api/post/post.js
+++ src/api/post/post.js
@@ -11,3 +11,35 @@ export const getPosts = async(query)=> {
         console.error(error);
     }
 }
+
+export const addLike = async(postId) => {
+    let token = localStorage.getItem('token');
+    try {
+        const resposne = await fetch(`https://blog.kreosoft.space/api/post${postId}/like`, {
+            method: 'POST',
+            headers: {
+                'Accept':'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        });
+        return resposne;
+    } catch (error) {
+        console.error('Failed add like to post: '+error);
+    }
+}
+
+export const removeLike = async(postId) => {
+    let token = localStorage.getItem('token');
+    try {
+        const resposne = await fetch(`https://blog.kreosoft.space/api/post${postId}/like`, {
+            method: 'DELETE',
+            headers: {
+                'Accept':'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        });
+        return resposne;
+    } catch (error) {
+        console.error('Failed add like to post: '+error);
+    }
+}
